a8dc9d8a7018a2eaadf92a23040b7adb60d2d531
pogi <llenaresdiether@gmail.com>
2020-07-18 08:48:21 +0800

    jul-17-2020

--- src/app/manpower/manpower.component.ts
+++ src/app/manpower/manpower.component.ts
@@ -1,3 +1,4 @@
+import Swal from 'sweetalert2';
 import { Manpower } from './../classes/manpower';
 import { ManpowerService } from './../services/manpower.service';
 import { Component, OnInit, AfterContentChecked, ViewChild, ViewChildren, ElementRef, QueryList } from '@angular/core';
@@ -94,6 +95,12 @@ export class ManpowerComponent implements OnInit, AfterContentChecked {
     .then (
       res => {
         this.accounts = res;
+        this.accounts.unshift({
+          ID: 0,
+          FIRST_NAME: 'No Manpower Setup',
+          LAST_NAME: '',
+          DISABLED: 0,
+        });
       }
     );
     if (!this.manpowers) {
@@ -102,18 +109,19 @@ export class ManpowerComponent implements OnInit, AfterContentChecked {
     this.manpowers.forEach((el , i) => {
       if (el.MANPOWER_ID > 0) {
         const accIndex = this.accounts.findIndex(x => x.ID === el.MANPOWER_ID);
-        this.accounts[accIndex].DISABLED = 1;
-        this.positions[i].selected = el.MANPOWER_ID;
-        this.positions[i].selected_index = accIndex;
+        if (accIndex !== -1) {
+          this.accounts[accIndex].DISABLED = 1;
+          this.positions[i].selected = el.MANPOWER_ID;
+          this.positions[i].selected_index = accIndex;
+        }
       }
     });
   }
 
   handleChange(event, i) {
-    const currValIndex = event.srcElement.attributes.selected_index.value;
+    const currValIndex = parseInt(event.srcElement.attributes.selected_index.value, 10);
     const options = event.srcElement.options;
-    const newValIndex = options.selectedIndex === 1 ? options.selectedIndex : options.selectedIndex - 1;
-    console.log(newValIndex);
+    const newValIndex = options.selectedIndex;
     if (currValIndex > 0) {
       this.accounts[currValIndex].DISABLED = 0;
     }
@@ -121,30 +129,27 @@ export class ManpowerComponent implements OnInit, AfterContentChecked {
     if (newValIndex > 0) {
       this.accounts[newValIndex].DISABLED = 1;
     }
+    if (newValIndex === 0) {
+      this.manpowers[i].START_TIME = '';
+      this.manpowers[i].END_TIME = '';
+      this.manpowers[i].REMARKS = '';
+    }
     this.manpowers[i].setManpowerID(this.positions[i].selected);
     this.handleInput(i);
   }
 
-  limit(val, key) {
-    const pattern = /^['0-9']$/i;
-    const count = val.toString().length;
-
-    if (pattern.test(key)) {
-      if (count === 4) {
-        return false;
-      }
-    }
-  }
-
-  time(value: string, event) {
+  async time(value: string, event, index: number, input: string) {
     const editable = this.tdEditable.toArray();
 
-    const active = editable.findIndex(index => {
-      return (index.nativeElement.parentElement === event.target.parentElement);
+    const active = editable.findIndex(el => {
+      return (el.nativeElement.parentElement === event.target.parentElement);
     });
 
     if (active < editable.length - 1) {
+      const hasErrors = await this.validateInput(event, index, input, 'enter');
+      if (!hasErrors) {
         editable[active + 1].nativeElement.focus();
+      }
     } else {
       event.target.blur();
     }
@@ -169,6 +174,60 @@ export class ManpowerComponent implements OnInit, AfterContentChecked {
 
   handleInput(i: number) {
     this.manpowers[i].IS_CHANGED = 1;
+  }
+
+  async validateInput(event, index: number, input: string, reason: string) {
+    console.log(reason);
+    let fulltime: string;
+    let hasErrors = false;
+    let message: string;
+
+    if (input === 'start') {
+      fulltime = this.manpowers[index].START_TIME.toString();
+    } else {
+      fulltime = this.manpowers[index].END_TIME.toString();
+    }
+
+    if (fulltime.length > 0 && fulltime.length <= 4) {
+      const time = parseInt(fulltime.slice(0, 2), 10);
+      const min = parseInt(fulltime.slice(2, 4), 10);
+      const mntTime = moment(moment().format('MM/DD/YYYY') + ' ' + time + ':' + min);
+
+      if (!mntTime.isValid()) {
+        hasErrors = true;
+        message = 'Invalid Time';
+      }
+
+      if (input === 'end') {
+        const start = this.manpowers[index].START_TIME.toString();
+        const startTime = parseInt(start.slice(0, 2), 10);
+        const startMin = parseInt(start.slice(2, 4), 10);
+        const startMntTime = moment(moment().format('MM/DD/YYYY') + ' ' + startTime + ':' + startMin);
+
+        if (mntTime.isBefore(startMntTime)) {
+          hasErrors = true;
+          message = 'End time must be after start time';
+        }
+      }
+
+      if (hasErrors) {
+        if (input === 'start') {
+          this.manpowers[index].START_TIME = '';
+        } else {
+          this.manpowers[index].END_TIME = '';
+        }
+        await Swal.fire({
+          title: 'Warning',
+          text: message,
+          icon: 'warning',
+          confirmButtonText: 'OK',
+        });
+        if (reason === 'blur') {
+          event.target.focus();
+        }
+      }
+    }
+    return hasErrors;
   }
 
 }
